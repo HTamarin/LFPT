250bce9baf327c16be5ad10a2fab55dfe992f747
St0rmsnake <dofen59@gmail.com>
2022-01-27 11:43:20 +0100

    new script without database create

--- script.sql
+++ script.sql
@@ -18,8 +18,7 @@
 --
 -- Table structure for table `game`
 --
-CREATE DATABASE `supereuro`;
-USE `supereuro`;
+
 DROP TABLE IF EXISTS `game`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
